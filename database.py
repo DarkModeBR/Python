eb52e0a8b2d115797a51a1b6e74fcd222baa63bf
DarkMode <darkmod3@tutamail.com>
2026-04-11 17:58:07 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,11 @@
 import os
 import mysql.connector
 
-
 def conectar():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST",     "square-cloud-db-01bd976874664d4b80775f7386d40abe.squareweb.app"),
+        host=os.getenv("DB_HOST", "square-cloud-db-01bd976874664d4b80775f7386d40abe.squareweb.app"),
         port=int(os.getenv("DB_PORT", "7200")),
-        user=os.getenv("DB_USER",     "squarecloud"),
-        password=os.getenv("DB_PASS", "YOUR_DB_PASSWORD"),
+        user=os.getenv("DB_USER", "squarecloud"),
+        password=os.getenv("DB_PASS", "626wMS71k1qWsCadpnnnmlyu"),
         database=os.getenv("DB_NAME", "railway"),
     )
